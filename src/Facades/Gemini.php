ab9619c9275022f80ea6120901eac4d20978bfd5
Fatih Aydın <fatihaydin@temizlikyolda.com>
2024-02-11 19:54:35 +0300

    refactor: Added docbloc to `Gemini` facade

--- src/Facades/Gemini.php
+++ src/Facades/Gemini.php
@@ -5,9 +5,18 @@ declare(strict_types=1);
 namespace Gemini\Laravel\Facades;
 
 use Gemini\Contracts\ResponseContract;
+use Gemini\Enums\ModelType;
 use Gemini\Laravel\Testing\GeminiFake;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Gemini\Resources\GenerativeModel generativeModel(ModelType $model)
+ * @method static \Gemini\Resources\GenerativeModel geminiPro()
+ * @method static \Gemini\Resources\GenerativeModel geminiProVision()
+ * @method static \Gemini\Resources\GenerativeModel embeddingModel(ModelType $model = ModelType::EMBEDDING)
+ * @method static \Gemini\Resources\Models models()
+ * @method static \Gemini\Resources\ChatSession chat(ModelType $model = ModelType::GEMINI_PRO)
+ */
 final class Gemini extends Facade
 {
     /**
